53f077383bad5cb09e28d18f8a3ee7c6b786230a
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2019-11-30 14:56:22 +0100

    Removed the summary.

--- Extra01 FizzBuzz/Program.cs
+++ Extra01 FizzBuzz/Program.cs
@@ -5,11 +5,7 @@ namespace Extra01_FizzBuzz
 {
     class Program
     {
-        /// <summary>
-        /// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
-        /// For numbers which are multiples of both three and five print "FizzBuzz".
-        /// Complexity: O(n)
-        /// </summary>
+        // Complexity O(n)
         static void Main(string[] args)
         {
             int n = 100;
