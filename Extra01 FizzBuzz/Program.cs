42883fc2943e86e9d97c3e333bce81307e34792e
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2019-11-30 14:50:59 +0100

    Added a random array generator.

--- Extra01 FizzBuzz/Program.cs
+++ Extra01 FizzBuzz/Program.cs
@@ -8,7 +8,7 @@ namespace Extra01_FizzBuzz
         /// <summary>
         /// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
         /// For numbers which are multiples of both three and five print "FizzBuzz".
-        /// O(n)
+        /// Complexity: O(n)
         /// </summary>
         static void Main(string[] args)
         {
---
+++ Extra02 Random Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace Extra02_Random_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 20;
+            int min = 7;
+            int max = 69;
+
+            int[] intArray = CreateRandomIntArray(length, min, max);
+            double[] doubleArray = CreateRandomDoubleArray(length, min, max);
+            StringBuilder stringBuilder1 = new StringBuilder(), stringBuilder2 = new StringBuilder();
+
+
+            for (int i = 0; i < length; i++) 
+            {
+                stringBuilder1.AppendLine($"{intArray[i]}");
+                stringBuilder2.AppendLine($"{doubleArray[i]}");
+            }
+
+            Console.WriteLine(stringBuilder1.ToString());
+            Console.WriteLine(stringBuilder2.ToString());
+        }
+
+        // Complexity O(n)
+        static int[] CreateRandomIntArray(int length, int min, int max) 
+        {
+            Random random = new Random();
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+                array[i] = random.Next(min, max);
+
+            return array;
+        }
+
+        // Complexity O(n)
+        static double[] CreateRandomDoubleArray(int length, int min, int max)
+        {
+            Random random = new Random();
+            double[] array = new double[length];
+
+            for (int i = 0; i < length; i++)
+                array[i] = random.Next(min, max) + random.NextDouble();
+
+            return array;
+        }
+    }
+}
