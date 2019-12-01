0ddb21284b51c4db867015bba73a46d27038b09a
Ossama Sijbesma (student) <s1120799@student.windesheim.nl>
2019-12-01 16:25:35 +0100

    Failed attempt to get a complexity of O(1)

---
+++ Extra05 Cumulative Sum/Algorithms.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Extra05_Cumulative_Sum
+{
+    class Algorithms
+    {
+        private int[] arrayCumalativeSum;
+
+        // Complexity O(n)
+        static int CumalativeSum(int[] array, int index) 
+        {
+            int total = 0;
+
+            for (int i = 0; i <= index; i++)
+                total += array[i];
+
+            return total;
+        }
+
+        static void InitCumalativeSum2(int[] array) 
+        {
+            
+        }
+
+        static int CumalativeSum2(int[] array, int index)
+        {
+            if (index < 0 || index > array.Length)
+                new IndexOutOfRangeException();
+
+
+        }
+
+        static void RunWithTimer(int[] array, int index)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            CumalativeSum(array, index);
+            stopWatch.Stop();
+
+            System.Console.WriteLine($"{stopWatch.ElapsedTicks} ticks");
+        }
+    }
+}
---
+++ Extra05 Cumulative Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extra05_Cumulative_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
