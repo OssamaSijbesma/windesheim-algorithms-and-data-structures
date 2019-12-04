862488abcfc7b3c31810e776a9dc9929b7f45aae
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2019-12-04 22:47:46 +0100

    Added unit tests.

---
+++ Lesson04 Tests/Ex1QuickSortTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson04_Tests
+{
+    [TestFixture]
+    public class Ex1QuickSortTests
+    {
+        [Test, Combinatorial]
+        public void Sort(
+                    [Values("QuickSort")] string sorterName,
+                    [Values(0, 10, 300)] int n)
+        {
+            List<int> list = new List<int>();
+            List<int> listCopy;
+            Sorter sorter;
+            System.Random random = new System.Random();
+
+            // Arrange
+            if (sorterName == "QuickSort")
+                sorter = DSBuilder.CreateQuickSorter();
+            else
+                sorter = null;
+            Assert.IsNotNull(sorter != null);
+
+            // Arrange
+            for (int i = 0; i < n; i++)
+                list.Add(random.Next(0, 100000));
+            listCopy = new List<int>(list);
+            listCopy.Sort();
+
+            // Act
+            sorter.Sort(list);
+
+            // Assert
+            bool equal = list.SequenceEqual(listCopy);
+            Assert.IsTrue(equal);
+        }
+    }
+}
---
+++ Lesson06 Tests/Ex1GraphTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson06_Tests
+{
+    [TestFixture]
+    public class Ex1GraphTests
+    {
+        private string StringWithoutSpaces(string s)
+        {
+            return Regex.Replace(s, @"\s+", "").Trim();
+        }
+
+        [Test]
+        public void Graph_ToString_OnEmptyGraph()
+        {
+            // Arrange
+            IGraph graph = DSBuilder.CreateGraphEmpty();
+            string expected = "";
+
+            // Act
+            string actual = StringWithoutSpaces(graph.ToString());
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Graph_ToString_OnGraph14_1()
+        {
+            // Arrange
+            IGraph graph = DSBuilder.CreateGraph14_1();
+            string expectedWithSpaces = StringWithoutSpaces("V0 [ V1(2) V3(1) ] V1 [ V3(3) V4(10) ] V2 [ V0(4) V5(5) ] V3 [ V2(2) V5(8) V6(4) V4(2) ] V4 [ V6(6) ] V5 V6 [ V5(1) ]");
+            string expected = StringWithoutSpaces(expectedWithSpaces);
+
+            // Act
+            string actual = StringWithoutSpaces(graph.ToString());
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+            Assert.Pass();
+        }
+    }
+}
